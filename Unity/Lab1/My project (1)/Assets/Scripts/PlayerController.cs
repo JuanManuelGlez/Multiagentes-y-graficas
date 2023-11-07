22791617929a2a1fa555c1658d2542eee1b06646
JuanManuelGlez <a00572003@tec.mx>
2023-11-06 21:41:42 -0600

    Lab 1 done, starting lab 2

--- Unity/Lab1/My project (1)/Assets/Scripts/PlayerController.cs
+++ Unity/Lab1/My project (1)/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    //Velocidad del vehiculo
+    public float speed = 5.0f;
+    //Variable para controlar el movimiento del vehiculo
+    public float turnSpeed = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Mover vehiculo para adelante
+        //transform.Translate(0,0,1)
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        //Modificar el giro
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed); 
     }
 }
